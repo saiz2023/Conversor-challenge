ead116366a6e62c0444a07481ed0b7f7ab51c97b
saiz2023 <129410040+saiz2023@users.noreply.github.com>
2023-07-14 17:26:08 -0500

    Update moneda.java
    agregando api de goolge

--- src/moneda.java
+++ src/moneda.java
@@ -2,6 +2,7 @@ import javax.swing.JOptionPane;
 
 public class moneda {
 	public void convercion(String seletMoneda ) {
+		api api=new api();
 		double valor=0;
 		int f=1;
 		String moneda = null;
@@ -16,59 +17,58 @@ public class moneda {
 		}
 		switch (seletMoneda) {
 		case "De pesos Colombianos a Dolares": {
-			valor*=0.00024;
+			valor=api.get("COP","USD",valor);
 			moneda=" Dolares";
 			break;
 			}
 		case "De pesos Colombianos a Euros":{
-			valor*=0.00022;
+			valor=api.get("COP","EUR",valor);
 			moneda=" Euros";
 			break;
 		}
 		case "De pesos Colombianos a Libras Esterlinas":{
-			valor*=0.00019;
+			valor=api.get("COP","GBP",valor);
 			moneda=" Libras Esterlinas";
 			break;
 			}
 		case "De pesos Colombianos a Yen Japones":{
-			valor*=0.035;
+			valor=api.get("COP","JPY",valor);
 			moneda=" Yen Japones";
 			break;
 			}
 		case "De pesos Colombianos a Won Surcoreano":{
-			valor*=0.32;
+			valor=api.get("COP","KRW",valor);
 			moneda=" Wones Surcoreanos";
 			break;
 		}
 		case "De Dolares a Pesos Colombianos":{
-			valor*=4155.22;
+			valor=api.get("USD","COP",valor);
 			moneda=" Pesos Colombianos";
 			break;
 		}
 		case "De euros a Pesos Colombianos":{
-			valor*=4536.01;
+			valor=api.get("EUR","COP",valor);
 			moneda=" Pesos Colombianos";
 			break;
 		}
 		case "De Libras Esterlinas a Pesos Colombianos":{
-			valor*=5279.82;
+			valor=api.get("GBP","COP",valor);
 			moneda=" Pesos Colombianos";
 			break;
 		}
 		case "De Yen Japones a Pesos Colombianos":{
-			valor*=28.79;
+			valor=api.get("JPY","COP",valor);
 			moneda=" Pesos Colombianos";
 			break;
 		}
 		case "De Won Surcoreano a Pesos Colombianos":{
-			
-			valor*=3.16;
+			valor=api.get("KRW","COP",valor);
 			moneda=" Pesos Colombianos";
 			break;
 		}
 		}
 		
-		JOptionPane.showMessageDialog(null,"Tienes $"+ (Math.round(valor * 10000.0) / 10000.0)+moneda);
+		JOptionPane.showMessageDialog(null,"Tienes $"+ valor+moneda);
 
 		
 	}
